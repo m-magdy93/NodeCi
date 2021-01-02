f71f3bf95bc1f5669ad20c939a31f5e10b5eada7
magdy <mohamed.magdy@hoopoe.digital>
2021-01-02 08:37:35 +0200

    edit readme

--- README.md
+++ README.md
@@ -1,22 +1,2 @@
 # AdvancedNodeStarter
 Starting project for a course on Advanced Node @ Udemy
-
-
-# Travis CI Flow
-  - We push code to github
-  - Travis automatically detects pushed code
-  - Travis clones the project
-  - Travis run tests using .travis.yaml file
-  - If tests are ok, Travis sends us an email or do as we wanted
-
-# Trvis file
-language: node_js     
-node_js:
-  - "12"        // node version
-dist: trusty    // used linux dist as a virtual machine
-services:
-  - mongodb
-  - redis-server
-env:
-  - NODE_ENV=ci
-  
--- tests/header.test.js
+++ tests/header.test.js
@@ -71,3 +71,25 @@ test('when signed in shows logout button', async () => {
    * KeyGrip.verify('session='+session, 'session.sig');
    * // returns true if not manipulated
    */
+
+   /**
+    * 
+    * 
+    # Travis CI Flow
+      - We push code to github
+      - Travis automatically detects pushed code
+      - Travis clones the project
+      - Travis run tests using .travis.yaml file
+      - If tests are ok, Travis sends us an email or do as we wanted
+
+    # Trvis file
+    language: node_js     
+    node_js:
+      - "12"        // node version
+    dist: trusty    // used linux dist as a virtual machine
+    services:
+      - mongodb
+      - redis-server
+    env:
+      - NODE_ENV=ci
+    */
